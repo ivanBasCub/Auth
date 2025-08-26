0b412bea541de0fbce593c03b7d2b4ba5e9d9be6
Ivan Bascones <ibascub2017@hotmail.com>
2025-08-26 15:51:43 +0200

    Creacion de filtros por rango

--- web/static/js/doctrineFilter.js
+++ web/static/js/doctrineFilter.js
@@ -1,13 +1,31 @@
 document.addEventListener("DOMContentLoaded", () =>{
     const input = document.getElementById("docTable_filter")
-    const filas = document.querySelectorAll("#docTable tbody tr");
+    const select =document.getElementById("docTable_rango_filter")
+    const tbody = document.querySelector('#docTable tbody');
+
 
     input.addEventListener("input", () => {
         const filtro = input.value.toLowerCase().trim()
+        const filas = tbody.querySelectorAll('tr');
 
         filas.forEach(fila => {
             const nombre = fila.querySelector("td:first-child").innerText.toLowerCase();
             fila.style.display = nombre.includes(filtro) ? "" : "none";
         });
     })
+
+    select.addEventListener('change', function () {
+        const filtro = this.value.trim();
+        const filas = tbody.querySelectorAll('tr');
+
+        filas.forEach(fila => {
+            const categoria = fila.cells[1].textContent.trim(); 
+            if (filtro === '' || categoria === filtro) {
+                fila.style.display = '';
+            } else {
+                fila.style.display = 'none';
+            }
+        });
+    });
+
 })
